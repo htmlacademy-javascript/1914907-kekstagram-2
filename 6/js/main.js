fed5da389e2f6bad707d9c51a4d6f42d072874b9
Keks <keks@htmlacademy.ru>
2026-06-19 16:27:09 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,23 @@
+import { generatePhotos } from './generators.js';
+
+const photos = generatePhotos();
+
+function renderPhotos() {
+  const container = document.getElementById('photos-container');
+
+  photos.forEach((photo) => {
+    const photoElement = document.createElement('div');
+    photoElement.className = 'photo-card';
+    photoElement.innerHTML = `
+      <h3>${photo.description}</h3>
+      <p> ${photo.likes} лайков</p>
+      <p> ${photo.comments.length} комментариев</p>
+      <p> ${photo.url}</p>
+    `;
+    container.appendChild(photoElement);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', renderPhotos);
+
+export { photos };
